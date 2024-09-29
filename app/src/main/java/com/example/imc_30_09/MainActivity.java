ebebe5a8cc4020223825601d17d319258afd7bd8
Imc <filipipires12@gmail.com>
2024-09-28 22:22:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/imc_30_09/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.imc_30_09;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnEntrar;
+    private EditText textInputUsuario ;
+    private EditText textInputSenha;
+    private TextView textResultadoErro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        textInputSenha = findViewById(R.id.textInputSenha);
+        textInputUsuario = findViewById(R.id.textInputUsuario);
+        textResultadoErro = findViewById(R.id.textResultadoErro);
+
+
+        btnEntrar.setOnClickListener(View ->{
+            String usuarioInput = textInputUsuario.getText().toString();
+            String senhaInput = textInputSenha.getText().toString();
+            if(usuarioInput.equals("admin") && senhaInput.equals("admin")){
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+            }else{
+
+                textResultadoErro.setText("Por Favor Digite seus Dados Corretamente...");
+
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/imc_30_09/MainActivity3.java
@@ -0,0 +1,48 @@
+package com.example.imc_30_09;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private TextView textResultadoImc;
+    private ImageButton btnVoltar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+
+        textResultadoImc = findViewById(R.id.textResultadoImc);
+        btnVoltar =findViewById(R.id.btnVoltar);
+
+        textResultadoImc.setText("");
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String resultado = bundle.getString("resultado");
+            String resul = "Resultado IMC:\n" + resultado;
+            textResultadoImc.setText(resul);
+        }
+
+        btnVoltar.setOnClickListener(View ->{
+            finish();
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
